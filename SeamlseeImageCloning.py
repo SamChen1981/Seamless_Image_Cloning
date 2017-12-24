2f01cc05197d063bbb5042796eb00302e2419b4c
passerbyYZC <yaozhichao418@163.com>
2017-12-24 12:29:20 +0800

    Update SeamlseeImageCloning.py

--- SeamlseeImageCloning.py
+++ SeamlseeImageCloning.py
@@ -1,10 +1,9 @@
 import cv2
-import matplotlib.pyplot as plt
 
-im_cloned = cv2.imread("cloned.png", cv2.IMREAD_GRAYSCALE)
-im_mask = cv2.imread("mask.png", cv2.IMREAD_GRAYSCALE)
-it = 200  # Set number of iterations
-
+im_cloned = cv2.imread("cloned.png", cv2.IMREAD_GRAYSCALE)  # 以灰度图形式读取克隆后的图片
+im_mask = cv2.imread("mask.png", cv2.IMREAD_GRAYSCALE)  # 以灰度图形式读取克隆后的图片的mask
+it = 200  # 设置迭代次数
+# 雅可比迭代法
 im_temp = im_cloned.copy()
 im_seamless = im_temp.copy()
 sigma = []
@@ -12,7 +11,7 @@ for i in range(im_cloned.shape[0]):
         for j in range(im_cloned.shape[1]):
             if im_mask[i, j] == 255:
                 sigma.append([i, j])
-
+# 平滑克隆区域边沿
 for a in range(it):
     for [i, j] in sigma:
         term = 10000
@@ -29,16 +28,9 @@ for a in range(it):
     im_seamless = im_temp.copy()
     if a % 50 == 0:
         print(a)
-
-fig, ax = plt.subplots(1, 2)
-ax[0].imshow(im_cloned, cmap='gray')
-ax[0].set_title('Normal Cloning')
-ax[1].imshow(im_seamless, cmap='gray')
-ax[1].set_title('Seamless Cloning')
-
-cv2.namedWindow("Naive Cloning", 0)
-cv2.namedWindow("Seamless Cloning", 0)
-
+# 打印原始克隆后的图片和无缝克隆后的图片
+cv2.namedWindow("Naive Cloning", 0)  # 定义一个名为Naive Cloning大小可调节的输出窗口
+cv2.namedWindow("Seamless Cloning", 0)  # 定义一个名为Seamless Cloning大小可调节的输出窗口
 cv2.imshow("Naive Cloning", im_cloned)
 cv2.imshow("Seamless Cloning", im_seamless)
 cv2.waitKey(0)
